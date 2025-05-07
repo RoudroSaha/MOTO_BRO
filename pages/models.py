9b35b5e057f55554f302db4b7ac9047ef8d30c5d
Roudro Saha <roudro1538@gmail.com>
2025-05-08 04:08:48 +0600

    refactor(team): update team model and templates for better image handling
    - Added default image path and photo_url property to Team model
    - Updated templates to use photo_url for consistent image display
    - Enhanced CSS for better image responsiveness and styling
    - Removed redundant code and improved template readability

--- pages/models.py
+++ pages/models.py
@@ -1,17 +1,23 @@
 from django.db import models
 
-# Create your models here.
 class Team(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     designation = models.TextField(max_length=200)
-    image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    image = models.ImageField(upload_to='teams/%Y/%m/%d/', default='teams/default-profile.jpg')
     facebook = models.URLField(max_length=200, blank=True)
     twitter = models.URLField(max_length=200, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+    @property
+    def photo_url(self):
+        try:
+            url = self.image.url
+        except:
+            url = '/static/img/team/default-profile.jpg'
+        return url
 
    
